9eca0f47af4b8814133bf90896736cea54ac315f
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-21 00:03:05 +0300

    Made counter methods private.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,13 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
+  private
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
+
+  public
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
