c74bbbb2f2efc8316fcdad7ddd5689b0eed5b635
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-24 11:45:40 +0300

    Add routes for the correct URL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
+  # get 'likes/index'
+  # get 'likes/show'
+  # get 'likes/new'
+  # get 'likes/edit'
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'comments/index'
+  # get 'comments/show'
+  # get 'comments/new'
+  # get 'comments/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root "users#index"
+  get '/', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id/posts/:id', to: 'posts#show'
 end
