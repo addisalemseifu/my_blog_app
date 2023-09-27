bf50d11e1a7cafb015d1d5ae1f60404736aaa484
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-27 17:16:44 +0300

    Implement seggested changes by the reviewer.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_user, only: %i[index show new edit create]
   def index
     @user_id = params[:user_id]
     @user = User.find(@user_id)
@@ -7,7 +8,8 @@ class PostsController < ApplicationController
 
   def show
     @user_id = params[:user_id]
-    @post = Post.find(@user_id)
+    @user = User.find(@user_id)
+    @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
 
@@ -18,6 +20,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
 
     if @post.save
       redirect_to user_posts_path(current_user), notice: 'Post created successfully'
@@ -30,7 +34,6 @@ class PostsController < ApplicationController
   private
 
   def find_user
-    # @user = User.find(params[:user_id])
     @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
   end
 
