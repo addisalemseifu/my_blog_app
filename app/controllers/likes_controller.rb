a2299971f7d7b8aabc8b084da5fb7282266ed818
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-27 13:09:58 +0300

    Add a functionality to make a user to like a post.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,18 @@ class LikesController < ApplicationController
   def new; end
 
   def edit; end
+
+  def create
+    @like = Like.new
+    @like.author = current_user
+    @like.post = Post.find(params[:post_id])
+    @like.save
+    redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+  end
+
+  def destroy
+    @like = current_user.likes.find(params[:user_id])
+    @like.destroy
+    redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+  end
 end
